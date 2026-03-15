687e3e116f20e8609140872c7d486bf49cff8197
Jacqueline Sanchez <Jacquelinesanchez4712@gmail.com>
2026-03-15 00:15:16 -0500

    fixing the functionality of new game generator button, bug 4

--- app.py
+++ app.py
@@ -73,9 +73,10 @@ with col3:
     show_hint = st.checkbox("Show hint", value=True)
 
 if new_game:
-    st.session_state.attempts = 0
-    st.session_state.secret = random.randint(1, 100)
+    st.session_state.attempts = 1
+    st.session_state.secret = random.randint(low, high)
     st.success("New game started.")
+    st.session_state.status = "playing"
     st.rerun()
 
 if st.session_state.status != "playing":
